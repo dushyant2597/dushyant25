fb3729a9cd971702602d5138bc59706e7a9852c7
dushyant2597 <dushyantsharma250797@gmail.com>
2018-09-30 12:56:11 +0530

    second assignment

---
+++ assignment2.py
@@ -0,0 +1,26 @@
+#Q.1 print something
+print("hello world")
+#Q.2 concatinate strings
+a="java"
+b="python"
+c=a+b
+print (c)
+#Q.3 user defined variables
+a=input("enter the value of a:")
+b=input("enter the value of b:")
+print(a,b)
+#Q.4 print Let's Get started
+print("Let's Get Started")
+#Q.5 print the following values using placeholders
+s= "maths"
+subject = "hindi"
+marks = 45
+print(" %s %s %d"%(s,subject,marks))
+#find the area of circle with user defined radius
+pi=3.14
+r=float(input("enter the radius:"))
+a=pi*r*r
+print(a)
+
+              
+
